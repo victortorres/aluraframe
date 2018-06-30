d1617d424f94076d2126653be4ab586bc471cba8
Victor Torres <victor.h.c.torres@gmail.com>
2018-06-30 19:15:11 +0100

    adicionado funcinalidade para somar os volumes cadastrados e usado o conceito IIFE para fazer o calculo

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -36,6 +36,15 @@ class NegociacoesView{
             </tbody>
             
             <tfoot>
+                <td colspan="3"></td>
+                <td>${
+                    (function(){
+                        let total = 0;
+                        model.negociacoes.forEach(
+                            n => total += n.volume
+                        )
+                    })()
+                }</td>
             </tfoot>
         </table>        
         `;
