ca4c38834420a0fbf1a0b62054bb91be07d1db9e
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-01 16:06:16 +0100

    adicionado chamada ajax para realizar importacao de negociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -39,6 +39,45 @@ class NegociacaoController {
         this._limparFormulario();
     }
 
+    importarNegociacoes(){
+        //instancio o objeto para lidar com AJAX
+        let xhr = new XMLHttpRequest();
+        //configuro o endereco e o metodo que sera acessado via ajax
+        //se for um endereco externo, da web, deve estar o endereco completo,
+        //como neste caso o endereco eh local, esta apenas o caminho relativo
+        xhr.open('GET', 'negociacoes/semana');
+
+        /*configuracoes*/
+        xhr.onreadystatechange = () => {
+            //0: requisição ainda não iniciada
+            //1: conexão com o servidor estabelecida
+            //2: requisição recebida
+            //3: processando requisição
+            //4: requisição está concluída e a resposta está pronta            
+            if (xhr.readyState == 4){
+                //200-ok
+                if (xhr.status == 200){
+                    //1-o ajax retorna texto
+                    //2-eh necessario converter para uma lista de json
+                    //3-faco um map na lista para poder criar uma negociacao para cada item
+                    //4-para cada item que o map processar eu adiciono a lista
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+
+                    this._mensagem.texto = "Lista de negociações carregada com sucesso.";
+                } else {
+                    //do something
+                    this._mensagem.texto = "Não foi possivel carregar lista de negociações.";
+                }
+            }
+        }
+        /*configuracoes*/
+
+        //processa a requisicao
+        xhr.send();
+    }
+
     _criarNegociacao(){
         return new Negociacao(  DateHelper.textoParaData(this._inputData.value), 
                                 this._inputQuantidade.value, 
