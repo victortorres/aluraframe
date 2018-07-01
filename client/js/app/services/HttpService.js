5052c94345d0c5fee449c13854c6076abbe345cd
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-01 20:45:15 +0100

    refactoring para separar a responsabilidade da chamada ajax em uma outra classe

---
+++ client/js/app/services/HttpService.js
@@ -0,0 +1,43 @@
+class HttpService{
+
+    get(url){
+
+        //1-retorna uma funcao que executara em caso de sucesso
+        //2-retorna uma mensagem de erro em caso de erro
+        return new Promise((resolve, reject) => {
+            //instancio o objeto para lidar com AJAX
+            let xhr = new XMLHttpRequest();
+            //configuro o endereco e o metodo que sera acessado via ajax
+            //se for um endereco externo, da web, deve estar o endereco completo,
+            //como neste caso o endereco eh local, esta apenas o caminho relativo
+            xhr.open('GET', url);
+    
+            /*configuracoes*/
+            xhr.onreadystatechange = () => {
+                //0: requisição ainda não iniciada
+                //1: conexão com o servidor estabelecida
+                //2: requisição recebida
+                //3: processando requisição
+                //4: requisição está concluída e a resposta está pronta            
+                if (xhr.readyState == 4){
+                    //200-ok
+                    if (xhr.status == 200){
+                        //1-o ajax retorna texto
+                        //2-eh necessario converter para uma lista de json
+                        resolve(JSON.parse(xhr.responseText));
+    
+                        //this._mensagem.texto = "Lista de negociações carregada com sucesso.";
+                    } else {
+                        //do something
+                        reject(xhr.responseText);
+                    }
+                }
+            }
+            /*configuracoes*/
+    
+            //processa a requisicao
+            xhr.send();
+        });
+
+    }
+}
--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,99 +1,63 @@
 class NegociacaoService{
 
+    constructor(){
+        this._http = new HttpService();
+    }
+
     obterNegociacaoDaSemana(){
-        //1-retorna uma funcao que executara em caso de sucesso
-        //2-retorna uma mensagem de erro em caso de erro
+        //este promise esta recebendo um outro promise
         return new Promise((resolve, reject) => {
-            //instancio o objeto para lidar com AJAX
-            let xhr = new XMLHttpRequest();
-            //configuro o endereco e o metodo que sera acessado via ajax
-            //se for um endereco externo, da web, deve estar o endereco completo,
-            //como neste caso o endereco eh local, esta apenas o caminho relativo
-            xhr.open('GET', 'negociacoes/semana');
-    
-            /*configuracoes*/
-            xhr.onreadystatechange = () => {
-                //0: requisição ainda não iniciada
-                //1: conexão com o servidor estabelecida
-                //2: requisição recebida
-                //3: processando requisição
-                //4: requisição está concluída e a resposta está pronta            
-                if (xhr.readyState == 4){
-                    //200-ok
-                    if (xhr.status == 200){
-                        //1-o ajax retorna texto
-                        //2-eh necessario converter para uma lista de json
-                        //3-faco um map na lista para poder criar uma negociacao para cada item
-                        resolve(JSON.parse(xhr.responseText)
-                                .map(objeto => new Negociacao(  new Date(objeto.data), 
-                                                                objeto.quantidade, 
-                                                                objeto.valor)));
-    
-                        //this._mensagem.texto = "Lista de negociações carregada com sucesso.";
-                    } else {
-                        //do something
-                        //this._mensagem.texto = "Não foi possivel carregar lista de negociações.";
-                        reject('Não foi possível importar as negociações da semana.')
-                    }
-                }
-            }
-            /*configuracoes*/
-    
-            //processa a requisicao
-            xhr.send();
+
+            this._http
+                .get('negociacoes/semana')
+                .then(negociacoes => {
+                    //essa lista ja foi tratada na promise anterior
+                    //porem ela vem como um objeto json simples
+                    //e aqui eu converto para uma outra classe
+                    resolve(negociacoes.map(
+                        objeto => new Negociacao(   new Date(objeto.data), 
+                                                    objeto.quantidade, 
+                                                    objeto.valor)));
+                })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível carregar as negociações da semana.');
+                });
         });
     }
 
     obterNegociacaoDaSemanaAnterior(){
-        
         return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-    
-            xhr.open('GET', 'negociacoes/anterior');
-    
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4){
-                    if (xhr.status == 200){
-    
-                        resolve(JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(  new Date(objeto.data), 
-                                                        objeto.quantidade, 
-                                                        objeto.valor)));
-    
-                    } else {
-                        reject('Não foi possível importar as negociações da semana anterior.')
-                    }
-                }
-            }
-    
-            xhr.send();
+            this._http
+                .get('negociacoes/anterior')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(
+                        objeto => new Negociacao(   new Date(objeto.data), 
+                                                    objeto.quantidade, 
+                                                    objeto.valor)));
+                })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível carregar as negociações da semana anterior.');
+                });
         });
     }
 
     obterNegociacaoDaSemanaRetrasada(){
         return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-    
-            xhr.open('GET', 'negociacoes/retrasada');
-    
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4){
-                    if (xhr.status == 200){
-    
-                        resolve(JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(  new Date(objeto.data), 
-                                                        objeto.quantidade, 
-                                                        objeto.valor)));
-    
-                    } else {
-                        reject('Não foi possível importar as negociações da semana retrasada.')
-                    }
-                }
-            }
-    
-            xhr.send();
-        });
+            this._http
+                .get('negociacoes/retrasada')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(
+                        objeto => new Negociacao(   new Date(objeto.data), 
+                                                    objeto.quantidade, 
+                                                    objeto.valor)));
+                })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível carregar as negociações da semana retrasada.');
+                });
+        });        
     }    
 }
