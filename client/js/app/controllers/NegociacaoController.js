f4d1d8f805d32f9e868c943cfd509478480ed070
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-01 20:10:25 +0100

    refactoring no uso de promises para fazer de uma forma mais enxuta

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -42,25 +42,30 @@ class NegociacaoController {
     importarNegociacoes(){
         let negociacaoService = new NegociacaoService();
 
-        negociacaoService.obterNegociacaoDaSemana()
+        //a promise.all recebe um array de promises, e a ordem que for passado
+        //esse array sera a ordem que sera tratado, ou seja, o proximo item da
+        //lista soh eh processado apos o anterior terminar, e eh centralizado
+        //em um unico lugar o que deve ser feito com as promises que retornarnarem
+        //com sucesso e com as que retornarem com erro
+        Promise.all(
+            [negociacaoService.obterNegociacaoDaSemana(),
+             negociacaoService.obterNegociacaoDaSemanaAnterior(),
+             negociacaoService.obterNegociacaoDaSemanaRetrasada()]
+        )
+        //retorno com sucesso
         .then(negociacoes => {
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            //os retornos do array de promises gera um array de retornos,
+            //e neste caso cada promisse retorno um array, entao devo tratar
+            //os arrays internos primeiro, para gerar um array simples, essa
+            //operacao eh chamada de flating, pois estao achatando o array de
+            //arrays em um unico array, e entao depois eh soh processar os itens
+            //do array no que precisar
+            negociacoes
+            .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
             this._mensagem.texto = 'Negociações da semana carregadas com sucesso.';
         })
-        .catch(erro => this._mensagem.texto = erro);
-        
-        negociacaoService.obterNegociacaoDaSemanaAnterior()
-        .then(negociacoes => {
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações da semana anterior carregadas com sucesso.';
-        })
-        .catch(erro => this._mensagem.texto = erro);
-        
-        negociacaoService.obterNegociacaoDaSemanaRetrasada()
-        .then(negociacoes => {
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações da semana retrasada carregadas com sucesso.';
-        })
+        //retorno com erro
         .catch(erro => this._mensagem.texto = erro);
 
     }
